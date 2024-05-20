08ead6a5e5e895725d82ff36965a805a1f5acc54
jawid fakhari <jawid_fakhari@live.it>
2024-05-20 11:33:41 +0200

    Add project files.

---
+++ ExceptionManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandler
+{
+    public class ExceptionManager
+    {
+        static int[] numbers = { 1, 8, 12, 5 };
+        public static bool WrongAnswer = false;
+
+        public static int GetNumber(int index)
+        {
+            // 2a- per evitare errore string della funzione GetNumber che deve ritornare solo int, creiamo un int output che ritorna infine l'output nel caso di exception
+            int output = -1;
+            // 1a- usiamo try catch + finally per gestire exceptions
+            try
+            {
+                output = numbers[index];
+
+            }
+            // 1b- usando Exception class che fa parte del System library e diamo il nome "ex"
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            //2b- finally sempre viene eseguito e fa parte di try e catch e qui se condizione output combacia allora ci da un error massage(che abbiamo scritto in program) e boolean ritorna true altrimenti boolean rimane falso
+            finally
+            {
+                if (output == -1)
+                {
+                    WrongAnswer = true;
+                }
+                else
+                {
+                    WrongAnswer = false;
+                }
+            }
+            return output;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using ExceptionHandler;
+
+Console.WriteLine("Please insert an index: ");
+int index = Int32.Parse(Console.ReadLine());
+
+int result = ExceptionManager.GetNumber(index);
+
+if (ExceptionManager.WrongAnswer)
+{
+    Console.WriteLine("The index was wrong");
+}
+else
+{
+    Console.WriteLine($"The number is: {result}");
+}
+
+Console.WriteLine("please prss a key to exit");
+Console.ReadLine();
