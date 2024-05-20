110edead387d8347d49d428f2f11791cdfeff6e1
jawid fakhari <jawid_fakhari@live.it>
2024-05-20 15:56:27 +0200

    Excersize 1

---
+++ Esercizio1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandler
+{
+    internal class Esercizio1
+    {
+        public static bool WrongNumber = false;
+        public double Calculate(double n1, double n2)
+        {
+            double output = -1;
+            if (n2 != 0)
+            {
+                double result = n1 / n2;
+                output = result;
+            }
+            else
+            {
+                if(output == -1)
+                {
+                    WrongNumber = true;
+                }
+                else
+                {
+                    WrongNumber = false;
+                }
+                //Console.WriteLine("Error: Can't devide by 0");
+            }
+            return output;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,18 +1,53 @@
 ﻿using ExceptionHandler;
 
-Console.WriteLine("Please insert an index: ");
-int index = Int32.Parse(Console.ReadLine());
+///////// Lezione
 
-int result = ExceptionManager.GetNumber(index);
+//Console.WriteLine("Please insert an index: ");
+//int index = Int32.Parse(Console.ReadLine());
 
-if (ExceptionManager.WrongAnswer)
+//int result = ExceptionManager.GetNumber(index);
+
+//if (ExceptionManager.WrongAnswer)
+//{
+//    Console.WriteLine("The index was wrong");
+//}
+//else
+//{
+//    Console.WriteLine($"The number is: {result}");
+//}
+
+//Console.WriteLine("please prss a key to exit");
+//Console.ReadLine();
+
+//1.Write a C# program that prompts the user to input two numbers and divides them. Handle an exception when the user enters non-numeric values.
+Esercizio1 esercizio1 = new Esercizio1();
+
+
+try
 {
-    Console.WriteLine("The index was wrong");
+    Console.WriteLine("Enter the number which you want to devide: ");
+    double n1 = Convert.ToDouble(Console.ReadLine());
+
+    Console.WriteLine("Enter the number of devision: ");
+    double n2 = Convert.ToDouble(Console.ReadLine());
+
+    double risutlato = esercizio1.Calculate(n1, n2);
+    if (Esercizio1.WrongNumber)
+    {
+        Console.WriteLine("Can not devide by 0");
+    }
+    else
+    {
+        Console.WriteLine($"result is: {risutlato}");
+
+    }
 }
-else
+catch (FormatException)
 {
-    Console.WriteLine($"The number is: {result}");
+    Console.WriteLine("Error: Non-numeric value entered.");
+}
+catch (Exception ex)
+{
+    Console.WriteLine("An error occurred: " + ex.Message);
 }
 
-Console.WriteLine("please prss a key to exit");
-Console.ReadLine();
